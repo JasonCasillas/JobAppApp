beaa91911c35d0eac96935736598fa2954a1ddc9
RageBarrage <jason.casillas@gmail.com>
2013-11-15 15:28:00 -0700

    New app.

---
+++ JobAppApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JobAppApp
+//
+//  Created by Jason Casillas on 11/15/13.
+//  Copyright (c) 2013 The World Needs Heroes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
